b0fb8281a024ca1f7f7c3a8af4cab4b82c3ff3ef
ixAbdulaziz <AzzMoh011@gmail.com>
2025-07-17 01:12:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -59,6 +59,4 @@ def search_suppliers():
 
 # الأمر التالي ضروري ليعمل التطبيق على Railway
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
